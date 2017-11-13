2d9b6a0aaab022d0865666142462d0e728f32ee1
Eugene Starodubov <eugene.starodubov@gmail.com>
2017-11-13 22:39:35 +0300

    v1.0.0.13 : New method() RegisterUser (90%), LoginUser(20%)

--- dance_api/api/config/database.php
+++ dance_api/api/config/database.php
@@ -6,6 +6,7 @@ class Database {
         $this->conn = null;
         try {
             $this->conn = new PDO("mysql:host=".$host.";dbname=".$db_name, $user_name, $password);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $this->conn->exec("set names utf8");
         } catch (PDOException $exception){
             echo "Connection error: ". $exception->getMessage();
